1665cbc20791464520a8ba164fa3790949def0e8
Tung-ht <httung2@cmcglobal.vn>
2022-05-19 14:05:36 +0700

    init project

---
+++ src/main/java/ict/group1/imovie/IMovieApiApplication.java
@@ -0,0 +1,13 @@
+package ict.group1.imovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMovieApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMovieApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ict/group1/imovie/IMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ict.group1.imovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMovieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
